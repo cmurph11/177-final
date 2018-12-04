f938dc8db97a0bb92bf1af4f940f5ceb528cd849
cmurph11 <colinmurphy@colin.local>
2018-12-03 21:01:02 -0500

    updated chart with quantized color rendition

---
+++ scores.js
@@ -0,0 +1,74 @@
+var scores = {
+    "2012": [
+        {
+            "SEPTEMBER": 75, 
+            "OCTOBER": 71.2, 
+            "NOVEMBER": 81.8, 
+            "DECEMBER": 62.5, 
+            "JANUARY": 61.9,
+            "FEBRUARY": 63.3,
+            "FINAL": 63.3
+        }, 
+    ], 
+
+    "2013": [
+        {
+            "SEPTEMBER": 50, 
+            "OCTOBER": 42.8, 
+            "NOVEMBER": 50, 
+            "DECEMBER": 50,
+            "FINAL": 50
+        }
+    ],
+    "2014": [
+        {
+            "SEPTEMBER": 75, 
+            "OCTOBER": 62.5, 
+            "NOVEMBER": 55.5, 
+            "DECEMBER": 62.5, 
+            "JANUARY": 61.1,
+            "FINAL": 61.1
+        }
+    ], 
+
+    "2015": [
+        {
+            "SEPTEMBER": 0, 
+            "OCTOBER": 0.16, 
+            "NOVEMBER": 36.3, 
+            "DECEMBER": 50, 
+            "JANUARY": 45.4,
+            "FINAL": 45.4
+        }
+    ], 
+
+    "2016": [
+        {
+            "SEPTEMBER": 100, 
+            "OCTOBER": 42.8, 
+            "NOVEMBER": 54.5, 
+            "DECEMBER": 53.3, 
+            "JANUARY": 50,
+            "FINAL": 50
+        }
+    ], 
+
+    "2017": [
+        {
+            "SEPTEMBER": 66, 
+            "OCTOBER": 50, 
+            "NOVEMBER": 54.5, 
+            "DECEMBER": 56.25,
+            "FINAL": 56.25
+        }
+    ], 
+
+    "2018": [
+        {
+            "SEPTEMBER": 75, 
+            "OCTOBER": 50, 
+            "NOVEMBER": 56.5,
+            "FINAL": 56.5
+        }
+    ]
+}
